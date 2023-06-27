1ca62740dbed9d526e47958e0c9e3a3bcb4ba68a
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-06-27 02:05:12 -0500

    Finish code for game

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const rpsList = ["rock", "paper", "scissors"];
 
-let getComputerChoice = (list) => list[Math.floor(Math.random() * 3)];
+let getComputerChoice = () => rpsList[Math.floor(Math.random() * 3)];
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection === computerSelection){
+        return("It's a draw!");
+    }
+    else if((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")){
+        return("You lose! " + computerSelection + " beats " + playerSelection + "!");
+    }
+    return("You win! " + playerSelection + " beats " + computerSelection + "!");
+}
+
+function game(list) {
+    for(let i = 1; i <= 5; i++){
+        let playerChoice = prompt("Choose one of the three: Rock, Paper, or Scissors")
+        console.log(playRound(playerChoice, getComputerChoice()));
+
+    }
+}
