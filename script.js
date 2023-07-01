cc8327a2c65bd387dd5dc03c81d71614cdf42f6a
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-06-30 19:53:24 -0500

    Add Winning scenario, reset button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const rpsList = ["rock", "paper", "scissors"];
 const selectors = document.querySelectorAll(".selection");
 const results = document.querySelector("#results");
-const computerScoreDiv = document.querySelector("#computerScore");
-const playerScoreDiv = document.querySelector("#playerScore");
+const computerScoreDiv = document.querySelector("#computer");
+const playerScoreDiv = document.querySelector("#player");
+const playAgain = document.querySelector("#playAgain");
+const winnerText = document.querySelector("#winnerText");
 let computerScore = 0;
 let playerScore = 0;
 
@@ -10,15 +12,31 @@ let playerScore = 0;
 let getComputerChoice = () => rpsList[Math.floor(Math.random() * 3)];
 
 selectors.forEach(selector => selector.addEventListener('click', (e) => {
-    results.textContent = (playRound(e.target.id, getComputerChoice()));
+    if(computerScore !== 5 && playerScore !== 5) results.textContent = (playRound(e.target.id, getComputerChoice()));
 }));
 
 function addPoints(winnerDiv, winnerScore){
     winnerScore++;
     winnerDiv.textContent = `Score: ${winnerScore}`;
+    if(winnerScore === 5) gameWon(winnerDiv.id);
     return winnerScore;
 }
 
+function gameWon(winner){
+    winnerScreen.style.display = "flex";
+    winnerText.textContent = `${winner} wins! Want to play again?`;
+}
+
+playAgain.addEventListener('click', () => resetGame());
+
+function resetGame() {
+    computerScore = 0;
+    playerScore = 0;
+    computerScoreDiv.textContent = "Score: 0";
+    playerScoreDiv.textContent = "Score: 0";
+    winnerScreen.style.display = "none";
+    results.textContent = "";
+}
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
