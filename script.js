0111ef33b01ff00275f2348a077f2cd6daa63edf
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-06-30 19:23:35 -0500

    Add score, change game code

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const rpsList = ["rock", "paper", "scissors"];
 const selectors = document.querySelectorAll(".selection");
 const results = document.querySelector("#results");
+const computerScoreDiv = document.querySelector("#computerScore");
+const playerScoreDiv = document.querySelector("#playerScore");
+let computerScore = 0;
+let playerScore = 0;
+
 
 let getComputerChoice = () => rpsList[Math.floor(Math.random() * 3)];
 
 selectors.forEach(selector => selector.addEventListener('click', (e) => {
     results.textContent = (playRound(e.target.id, getComputerChoice()));
 }));
+
+function addPoints(winnerDiv, winnerScore){
+    winnerScore++;
+    winnerDiv.textContent = `Score: ${winnerScore}`;
+    return winnerScore;
+}
 
 
 function playRound(playerSelection, computerSelection){
@@ -17,7 +28,9 @@ function playRound(playerSelection, computerSelection){
     else if((playerSelection === "rock" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "scissors") ||
       (playerSelection === "scissors" && computerSelection === "rock")){
-        return("You lose! " + computerSelection + " beats " + playerSelection + "!");
+        computerScore = addPoints(computerScoreDiv, computerScore);
+        return(`You lose! ${computerSelection} beats ${playerSelection}!`);
     }
-    return("You win! " + playerSelection + " beats " + computerSelection + "!");
+    playerScore = addPoints(playerScoreDiv, playerScore);
+    return(`You win! ${playerSelection} beats ${computerSelection}!`);
 }
